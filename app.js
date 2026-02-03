16334eb9d0ea190cab64ab10efb2d1214b3040c2
McDonald <wmcdonald@una.edu>
2026-02-03 13:14:29 -0600

    Added MongoDB connection

--- app.js
+++ app.js
@@ -1,17 +1,47 @@
 // app.js
 
 // We are in ES6, use this.
+import 'dotenv/config'; 
 import express from 'express';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import { MongoClient , ServerApiVersion} from 'mongodb';
 
 // to get __dirname in ES6
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const uri = process.env.MONGO_URI;  
 console.log('Current directory:', __dirname)
 console.log('Current file:', __filename)
 
 const app = express()
+
+app.use(express.static(join(__dirname, 'public')));
+app.use(express.json()); 
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("formare").command({ ping: 1 });
+    
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+const database = client.db("formare");
 
 // middlewares aka endpoints aka 'get to slash' {http verb} to slow {your path, endpoint}
 app.get('/', (req, res) => {
