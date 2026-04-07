4c44301faf0f2b5971a1bf2c11f632a57a81301c
armac7 <w.mcd0320@icloud.com>
2026-04-07 13:23:04 -0500

    Removed old files that got moved

--- routes/sessionRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { loggedIn } from '../middleware/session.js';
-
-const router = express.Router();
-
-router.get('/checkAuth', loggedIn);
-
-export default router;
