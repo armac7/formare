fff9d9ae04b0230475a9c65520c41b9030430db9
William Ryan McDonald <113379340+armac7@users.noreply.github.com>
2026-04-14 15:28:23 -0500

    Revert "Implement core React UI screens for Formare (Iss11)"

---
+++ client/public/scripts/auth/logout.js
@@ -0,0 +1,17 @@
+export async function logout() 
+{
+    try {
+        const res = await fetch('/auth/logout', {
+            method: "POST",
+            credentials: "include"
+        });
+        
+        if (!res.ok) throw new Error("Logout failed");
+
+        // console.log("Redirecting..."); //DEBUG
+        window.location.replace("/index.html?cachebust=" + Date.now());
+    } catch (err) 
+    {
+        console.error("Logout Error: ", err);
+    }
+}
